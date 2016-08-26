5a7294ca61ff4469a7388167126941c585c5a1e2
jramos31 <jramos04@nyit.edu>
2016-08-25 20:56:26 -0400

    Changes to server.js (#2)

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 
 var app = express(); //Creates the app
+const PORT = process.env.PORT || 3000; //Creates port for Heroku or to use localhost:3000
 
+app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'https'){
+        res.redirect("http://" + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
 
 app.use(express.static('public')); //Folder to serve
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
+});
